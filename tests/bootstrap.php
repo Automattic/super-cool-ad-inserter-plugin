c76e52480582e192a9771aefe742b8db68e67cb8
Ben Keith <ben@benlk.com>
2015-12-01 20:12:13 -0500

    Using our test bootstrap and phpunit files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,15 +3,35 @@
 $wp_tests_dir = getenv('WP_TESTS_DIR');
 require_once $wp_tests_dir . '/includes/functions.php';
 
-function _manually_load_environment() {
-	// Add your theme
-	switch_theme("your-theme-name");
+$basename = basename(dirname(__DIR__));
 
-	// Update array with plugins to include ...
-	$plugins_to_active = array("your-plugin/your-plugin.php");
+$GLOBALS['wp_tests_options'] = array(
+	'stylesheet' => $basename,
+	'template' => $basename
+);
+
+tests_add_filter('set_current_user', function($arg) {
+	$user = wp_get_current_user();
+	$user->set_role('administrator');
+	return $arg;
+}, 1, 10);
+
+tests_add_filter('filesystem_method', function($arg) {
+	return 'direct';
+}, 1, 10);
+
+function _manually_load_environment() {
+	$plugins_to_active = array (basename(dirname(__DIR__))) . "/analytic-bridge.php";
 
 	update_option('active_plugins', $plugins_to_active);
+
 }
 tests_add_filter('muplugins_loaded', '_manually_load_environment');
 
 require $wp_tests_dir . '/includes/bootstrap.php';
+
+if ( !function_exists('var_log') ) {
+	function var_log($stuff) {
+		error_log(var_export($stuff, true));
+	}
+}
