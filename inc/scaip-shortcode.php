3c06203a2f75ad28e0b005839de231631334e951
Ben Keith <ben@benlk.com>
2015-12-09 16:58:34 -0500

    Implements the action for #9 and the custom shortcode detection for #4

--- inc/scaip-shortcode.php
+++ inc/scaip-shortcode.php
@@ -6,9 +6,24 @@
  * @return HTML
  */
 function scaip_shortcode( $atts, $content, $tag) {
-	if ( isset($atts['number'])) {
-		$atts['number'] = "DAAAAAD!";
+	if ( isset($atts['no'])) {
+		return '';
 	}
-	return "<p>Super Cool Ad Inserter Plugin was here! This is shortcode number " . $atts['number'] . ". Sincerely, the SCAIP Goat.</p>";
+	ob_start();
+	do_action('scaip_shortcode', $atts);
+	$ret = ob_get_clean();
+	return $ret;
 }
 add_shortcode('scaip', 'scaip_shortcode');
+
+/**
+ * Dummy test function that does not harm users' content, outputs an HTML comment.
+ *
+ * To remove this action, remove_action('scaip_shortcode', 'scaip_shortcode_test_comment');
+ */
+function scaip_shortcode_test_comment($args) {
+	echo "<!-- SCAIP was here, with these arguments: ";
+	echo var_dump($args);
+	echo "-->";
+}
+add_action('scaip_shortcode', 'scaip_shortcode_test_comment');
