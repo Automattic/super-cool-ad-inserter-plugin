d5795233098881fee4efebd16affb428fda5da76
Adam Schweigert <aschweigert@gmail.com>
2016-06-09 12:36:36 -0400

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,154 +1,315 @@
-#!/bin/bash
+#!/usr/bin/env bash
+RELEASE_DIR=release;
+SVN_PATH=$RELEASE_DIR/svn;
+SVN_REPO="https://plugins.svn.wordpress.org/super-cool-ad-inserter/";
+BLACKLIST=(
+.\*
+release.sh
+mkdocs.yml
+phpunit.xml
+requirements.txt
+composer.lock
+composer.json
+package.json
+bower.json
+Gruntfile.js
+release/\*
+tests/\*
+node_modules/\*
+docs/\*
+);
 
-# bundle a release and push to WordPress plugin directory.
-# modified from https://github.com/publicmediaplatform/pmp-wordpress/blob/master/release.sh
+function ensure_release_dir() {
+  local release_dir="$1";
+  if [[ ! -d $release_dir ]]
+  then
+    echo "Creating $release_dir directory";
+    mkdir -p $release_dir;
+  fi
+}
 
-RELEASE_DIR=release
-SVN_PATH=$RELEASE_DIR/svn
-SVN_REPO="https://plugins.svn.wordpress.org/super-cool-ad-inserter/"
+function is_master_branch() {
+  local remotes=`git ls-remote --quiet`;
+  local current=`get_current_git_commit`;
+  local is_master=`echo "$remotes" | grep "refs/heads/master" | grep $current | awk '{print $2;}'`;
+  echo "$is_master";
+}
 
-# Simple colors
-ESC_SEQ="\x1b["
-COL_RESET=$ESC_SEQ"39;49;00m"
-COL_RED=$ESC_SEQ"31;01m"
-COL_GREEN=$ESC_SEQ"32;01m"
-COL_YELLOW=$ESC_SEQ"33;01m"
-COL_BLUE=$ESC_SEQ"34;01m"
-COL_MAGENTA=$ESC_SEQ"35;01m"
-COL_CYAN=$ESC_SEQ"36;01m"
+function is_git_tag() {
+  local remotes=`git ls-remote --quiet`;
+  local current=`get_current_git_commit`;
+  local is_tag=`echo "$remotes" | grep "refs/tags" | grep $current | awk '{print $2;}' | sed -e 's/^refs\/tags\///;s/\^{}$//'`;
+  echo "$is_tag";
+}
 
-# which files go into the release?
-GLOBIGNORE=*
-WHITELIST=(js/* scaip.php inc/* readme.txt)
-BLACKLIST=(docs/* tests/* .travis.yml Gruntfile.js phpunit.xml readme.md .* *.sh)
+function get_current_git_commit() {
+  echo `git rev-parse HEAD`;
+}
 
-# (1) check branch state of this git repo
-REMOTES=`git ls-remote --quiet`
-CURRENT=`git rev-parse HEAD`
-IS_MASTER=`echo "$REMOTES" | grep "refs/heads/master" | grep $CURRENT | awk '{print $2;}'`
-IS_TAG=`echo "$REMOTES" | grep "refs/tags" | grep $CURRENT | awk '{print $2;}'`
-if [[ $IS_MASTER == "" && $IS_TAG == "" ]]
+function check_repo_state() {
+  #
+  # Check the state of this git repo. If no tag is checked out
+  # and we're not on the master branch, bail.
+  #
+  local is_master=`is_master_branch`;
+  local is_tag=`is_git_tag`;
+
+  if [[ "$is_master" == "" && "$is_tag" == "" ]]
+  then
+    echo "Bad release state for git repo!";
+    echo "Make sure you've checked out a tag or the master branch before releasing.";
+    exit 1;
+  else
+    echo "Repository is ready for deployment...";
+  fi
+}
+
+function confirm_deployment() {
+  # make sure we know what we're doing
+  local is_master=`is_master_branch`;
+  local is_tag=`is_git_tag`;
+  local which_text="[master] and [$is_tag]";
+
+  if [[ $is_master == "" ]]
+  then
+    which_text="[$is_tag]";
+  fi
+
+  if [[ $is_tag == "" ]]
+  then
+     which_text="[master]";
+  fi
+
+  read -p "Really release plugin from $which_text? [y/N] " -r;
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+    echo "No changes made. Exiting...";
+    exit 0;
+  fi
+}
+
+function init_update_svn_repo() {
+  local svn_path="$1";
+  local svn_repo="$2";
+
+  if [[ "$svn_path" == "" || "$svn_repo" == "" ]]
+  then
+    echo "The svn_path and svn_repo arguments are required.";
+    exit 1;
+  fi
+
+  # init and update svn repo
+  if [[ ! -d $svn_path ]]
+  then
+    echo " - checking out svn repo";
+    OUT=`mkdir -p $svn_path && svn checkout $svn_repo $svn_path`
+    if [[ $? -ne 0 ]]
+    then
+      echo "$OUT";
+      exit 1;
+    fi
+  else
+    echo " - updating svn repo";
+    OUT=`cd $svn_path && svn update`;
+    if [[ $? -ne 0 ]]
+    then
+      echo "$OUT";
+      exit 1;
+    fi
+  fi
+}
+
+function create_release_zip() {
+  # Build a release zip file
+  echo "Creating release/wp-release.zip";
+
+  OUT=`rm -f release/wp-release.zip`;
+  OUT=`zip -x "${BLACKLIST[@]}" -q -r release/wp-release.zip .`;
+
+  if [[ $? -ne 0 ]]
+  then
+    echo "$OUT";
+    exit 1;
+  fi
+}
+
+function install_update_dependencies() {
+  echo "Checking for third-party/vendor dependencies...";
+  # If composer.json exists, run composer install
+  if [[ -f composer.json ]]
+  then
+    echo " - installing composer dependencies";
+    composer install --no-dev;
+  fi
+
+  # If package.json exists, run npm install
+  if [[ -f package.json ]]
+  then
+    echo " - installing npm dependencies";
+    npm install;
+  fi
+
+  # If bower.json exists, run bower install
+  if [[ -f bower.json ]]
+  then
+    echo " - installing bower dependencies";
+    bower install;
+  fi
+}
+
+function write_trunk() {
+  local svn_path="$1";
+  local is_master=`is_master_branch`;
+
+  if [[ $is_master != "" ]]
+  then
+    trunk_path=$svn_path/trunk;
+    echo "Writing to $trunk_path";
+
+    # overwrite with unzip
+    OUT=`rm -rf $trunk_path && unzip -o release/wp-release.zip -d $trunk_path`;
+    if [[ $? -ne 0 ]]
+    then
+      echo "$OUT";
+      exit 1;
+    fi;
+
+    # stage all changes (adds and removes)
+    OUT=`cd $trunk_path && svn st | grep '^\?' | awk '{print \$2}' | xargs svn add` # add all
+    if [[ $? -ne 0 ]]
+    then
+      echo "$OUT";
+      exit 1;
+    fi
+
+    OUT=`cd $trunk_path && svn st | grep '^\!' | awk '{print \$2}' | xargs svn rm` # remove all
+    if [[ $? -ne 0 ]]
+    then
+      echo "$OUT";
+      exit 1;
+    fi
+
+    # make sure something has changed besides the autoloader hashes
+    CHANGES=`cd $trunk_path && svn st | grep -v 'autoload\(_real\)\?\.php'`
+    if [[ $CHANGES == "" ]]
+    then
+      echo "There are no changes to commit for trunk.";
+      OUT=`cd $trunk_path && svn revert --recursive .`;
+      if [[ $? -ne 0 ]]
+      then
+        echo "$OUT";
+        exit 1;
+      fi
+    else
+      echo "Committing $trunk_path (slow) ...";
+      CURRENT=`get_current_git_commit`;
+      OUT=`cd $trunk_path && svn commit -m "update trunk to git $CURRENT"`;
+      if [[ $? -ne 0 ]]
+      then
+        echo "$OUT";
+        exit 1;
+      fi
+    fi
+  fi
+}
+
+function write_tag() {
+  local svn_path="$1";
+  local is_tag=`is_git_tag`;
+
+  if [[ $is_tag != "" ]]
+  then
+    WP_TAG=`echo $is_tag | sed -e 's/^v//'`;
+    TAG_PATH=$svn_path/tags/$WP_TAG;
+    echo "Writing to $TAG_PATH";
+
+    # overwrite with unzip
+    OUT=`rm -rf $TAG_PATH && unzip -o release/wp-release.zip -d $TAG_PATH`;
+    if [[ $? -ne 0 ]]
+    then
+      echo "$OUT";
+      exit 1;
+    fi
+
+    # TODO: set version numbers and/or ensure version numbers in plugin files are correct
+
+    # stage all changes (adds and removes)
+    OUT=`cd $svn_path/tags && svn st | grep '^\?' | awk '{print \$2}' | xargs svn add`; # add all
+    if [[ $? -ne 0 ]]
+    then
+      echo "$OUT";
+      exit 1;
+    fi
+    OUT=`cd $svn_path/tags && svn st | grep '^\!' | awk '{print \$2}' | xargs svn rm`; # remove all
+    if [[ $? -ne 0 ]]
+    then
+      echo "$OUT";
+      exit 1;
+    fi
+
+    # make sure something has changed besides the autoloader hashes
+    CHANGES=`cd $svn_path/tags && svn st | grep -v 'autoload\(_real\)\?\.php'`;
+    if [[ $CHANGES == "" ]]
+    then
+      echo "There are no changes to commit for $TAG_PATH";
+      OUT=`cd $svn_path/tags && svn revert --recursive .`;
+      if [[ $? -ne 0 ]]
+      then
+        echo "$OUT";
+        exit 1;
+      fi
+    else
+      echo "Committing $TAG_PATH (slow) ...";
+      CURRENT=`get_current_git_commit`;
+      OUT=`cd $svn_path/tags && svn commit -m "update $WP_TAG to git $CURRENT"`;
+      if [[ $? -ne 0 ]]
+      then
+        echo "$OUT";
+        exit 1;
+      fi
+    fi
+  fi
+}
+
+function help_text() {
+  echo "Usage: ./release.sh [--dry_run, --help]";
+  echo "";
+  echo "--dry_run: Create the release directory and release zip,
+           but bon't actually commit to the SVN repository."
+  echo "";
+  echo "--help:    Display this help screen and exit.";
+  echo "";
+  exit 0;
+}
+
+# Parse args
+if [[ $@ =~ "help" || $@ =~ "--help" ]]
 then
-  echo ""
-  echo ""
-  echo -e "${COL_RED}[Error]${COL_RESET}: Bad release state for git repo."
-  echo "Make sure you've checked out a git-tag or latest-master before releasing."
-  echo ""
-  echo ""
-  exit 1
+  help_text;
 fi
 
-# (2) check dirty/untracked state of this git repo
-IS_DIRTY=`git status --porcelain 2> /dev/null`
-if [[ $IS_DIRTY != "" ]]
+if [[ $@ =~ "dry_run" || $@ =~ "--dry_run" ]]
 then
-  echo ""
-  echo ""
-  echo -e "${COL_RED}[Error]${COL_RESET}: This branch is dirty, or has untracked changes."
-  echo "Commit or discard changes before release."
-  echo ""
-  echo ""
-  exit 1
-fi
-
-# (3) make sure we know what we're doing
-WHICH_TEXT="[master] and [$(echo $IS_TAG | sed -e 's/^refs\/tags\///')]"
-if [[ $IS_MASTER == "" ]]; then WHICH_TEXT="[$(echo $IS_TAG | sed -e 's/^refs\/tags\///')]"; fi
-if [[ $IS_TAG == "" ]]; then WHICH_TEXT="[master]"; fi
-read -p "Really release plugin from $WHICH_TEXT? " -n 1 -r
-echo ""
-echo ""
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-  echo "(no changes made)"
-  echo ""
-  exit 0
-fi
-
-# (4) init and update svn repo
-if [[ ! -d $SVN_PATH ]]
-then
-  echo " - checking out svn repo"
-  OUT=`mkdir -p $SVN_PATH && svn checkout $SVN_REPO $SVN_PATH`
-  if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
+  dry_run=1;
 else
-  echo " - updating svn repo"
-  OUT=`cd $SVN_PATH && svn update`
-  if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
+  dry_run=0;
 fi
 
-# (5) build zip
-echo " - zipping up release/wp-release.zip"
-OUT=`rm -f release/wp-release.zip`
-OUT=`zip -r release/wp-release.zip . --include ${WHITELIST[@]} --exclude ${BLACKLIST[@]} -q`
-if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
+# Start the release process
+ensure_release_dir "$RELEASE_DIR";
+check_repo_state;
+confirm_deployment;
 
-# (6) optionally write to /svn/trunk
-if [[ $IS_MASTER != "" ]]
+if [[ $dry_run == 0 ]]
 then
-  TRUNK_PATH=$SVN_PATH/trunk
-
-  # overwrite with unzip
-  echo " - writing to $TRUNK_PATH"
-  OUT=`rm -rf $TRUNK_PATH && unzip -o release/wp-release.zip -d $TRUNK_PATH`
-  if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
-
-  # stage all changes (adds and removes)
-  OUT=`cd $TRUNK_PATH && svn st | grep '^\?' | awk '{print \$2}' | xargs svn add` # add all
-  if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
-  OUT=`cd $TRUNK_PATH && svn st | grep '^\!' | awk '{print \$2}' | xargs svn rm` # remove all
-  if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
-
-  # make sure something has changed besides the autoloader hashes
-  CHANGES=`cd $TRUNK_PATH && svn st | grep -v 'autoload\(_real\)\?\.php'`
-  if [[ $CHANGES == "" ]]
-  then
-    echo "   no changes to commit for trunk"
-    OUT=`cd $TRUNK_PATH && svn revert --recursive .`
-    if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
-  else
-    echo " - committing $TRUNK_PATH (slow) ..."
-    OUT=`cd $TRUNK_PATH && svn commit -m "update trunk to git $CURRENT"`
-    if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
-  fi
+  init_update_svn_repo "$SVN_PATH" "$SVN_REPO";
 fi
 
-# (7) optionally write to /svn/tags/0.0.0
-if [[ $IS_TAG != "" ]]
+install_update_dependencies;
+create_release_zip;
+
+if [[ $dry_run == 0 ]]
 then
-  WP_TAG=`echo $IS_TAG | sed -e 's/^refs\/tags\///' -e 's/^v//'`
-  TAG_PATH=$SVN_PATH/tags/$WP_TAG
-
-  # overwrite with unzip
-  echo " - writing to $TAG_PATH"
-  OUT=`rm -rf $TAG_PATH && unzip -o release/wp-release.zip -d $TAG_PATH`
-  if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
-
-  # set the version
-  echo " - updating dfw.php version to $WP_TAG"
-  OUT=`sed "s/\* Version:.*$/* Version: $WP_TAG/" $TAG_PATH/dfw.php > dfw.php.new && mv dfw.php.new $TAG_PATH/dfw.php`
-  if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
-
-  # stage all changes (adds and removes)
-  OUT=`cd $SVN_PATH/tags && svn st | grep '^\?' | awk '{print \$2}' | xargs svn add` # add all
-  if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
-  OUT=`cd $SVN_PATH/tags && svn st | grep '^\!' | awk '{print \$2}' | xargs svn rm` # remove all
-  if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
-
-  # make sure something has changed besides the autoloader hashes
-  CHANGES=`cd $SVN_PATH/tags && svn st | grep -v 'autoload\(_real\)\?\.php'`
-  if [[ $CHANGES == "" ]]
-  then
-    echo "   no changes to commit for $TAG_PATH"
-    OUT=`cd $SVN_PATH/tags && svn revert --recursive .`
-    if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
-  else
-    echo " - committing $TAG_PATH (slow) ..."
-    OUT=`cd $SVN_PATH/tags && svn commit -m "update $WP_TAG to git $CURRENT"`
-    if [[ $? -ne 0 ]]; then echo "$OUT" && exit 1; fi
-  fi
+  write_trunk "$SVN_PATH";
+  write_tag "$SVN_PATH";
 fi
-
-# success
-echo ""
-echo -e "${COL_GREEN}all done!${COL_RESET}"  
