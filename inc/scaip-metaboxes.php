a0b0b0ad6fb4f5159c5fe0e6487bd6bf7a268ea3
aschweigert <aschweigert@gmail.com>
2017-06-28 17:10:02 -0400

    address issues in inc/metaboxes.php

--- inc/scaip-metaboxes.php
+++ inc/scaip-metaboxes.php
@@ -3,36 +3,31 @@
 /**
  * Short explanation of what the SCAIP shortcode is and how to use it in posts.
  */
-function scaip_how_to_shortcode_callback( $post = null ) {
-	$post = get_post( $post );
+function scaip_how_to_shortcode_callback() {
+	global $post;
 
 	$scaip_period = get_option( 'scaip_settings_period', 3 );
 	$scaip_repetitions = get_option( 'scaip_settings_repetitions', 2 );
 	$scaip_minimum_paragraphs = get_option( 'scaip_settings_min_paragraphs', 6 );
-
+	wp_nonce_field( 'scaip_metabox', 'scaip_metabox_nonce' );
 	?>
 	<p>
 		<?php
 			printf(
-				__( 'By default, %1$s ads will be inserted in a post, beginning %2$s paragraphs after the beginning and every %2$s paragraphs after that. They will not appear if this post is shorter than %3$s paragraphs long.', 'scaip' ),
-				$scaip_repetitions,
-				$scaip_period,
-				$scaip_minimum_paragraphs
+				esc_html__( 'By default, %1$s ads will be inserted in a post, beginning %2$s paragraphs after the beginning and every %2$s paragraphs after that. They will not appear if this post is shorter than %3$s paragraphs long.', 'scaip' ),
+				esc_html( $scaip_repetitions ),
+				esc_html( $scaip_period ),
+				esc_html( $scaip_minimum_paragraphs )
 			);
 		?>
 	</p>
-	<p><?php _e( 'If the automatic positioning is terrible, you can <a href="https://github.com/INN/super-cool-ad-inserter-plugin/blob/master/docs/display-settings.md">prevent automatic placement of the ads</a>.', 'scaip' );?></p>
+	<p>
+		<?php echo wp_kses( __( 'If the automatic positioning causes problems for any given post, you can prevent automatic placement of the ads <a href="https://github.com/INN/super-cool-ad-inserter-plugin/blob/master/docs/display-settings.md">using a shortcode</a> or disable them completely by checking this box:', 'scaip' ), array( 'a' => array( 'href' => array() ) ) ); ?>
+	</p>
 	<?php
-
-	// Only show the override option on posts.
-
-	if ( $post != null && current_user_can( 'edit_others_posts' ) ) {
-		$value = get_post_meta( $post->ID, 'scaip_prevent_shortcode_addition', true );
-		?>
-		<?php _e( '<p>If you want to disable automatic addition of ads, disable it here:</p>', 'scaip' ); ?>
-		<p><input type='checkbox' class='checkbox' id='scaip_prevent_shortcode_addition' name='scaip_prevent_shortcode_addition' <?php checked( $value, 'on' ); ?> /><label for='scaip_prevent_shortcode_addition'><?php _e( 'Prevent automatic addition of ads to this post.', 'scaip' ); ?></label></p>
-
-		<?php
+	if ( current_user_can( 'edit_others_posts' ) ) {
+		$checked = ( 1 === intval( get_post_meta( $post->ID, 'scaip_prevent_shortcode_addition', true ) ) ) ? 'checked="checked"' : '';
+		echo '<p><label class="selectit"><input type="checkbox" value="true" name="scaip_prevent_shortcode_addition"' . esc_attr( $checked ) . '> ' . esc_html__( 'Prevent automatic addition of ads to this post.', 'scaip' ) . '</label></p>';
 	}
 }
 
@@ -44,14 +39,14 @@ add_action( 'add_meta_boxes', function() {
 });
 
 // But always register the meta.
-register_meta( 'post', 'scaip_prevent_shortcode_addition', 'scaip_prevent_shortcode_addition_sanitize' );
+register_meta( 'post', 'scaip_prevent_shortcode_addition', array( 'sanitize_callback' => 'scaip_prevent_shortcode_addition_sanitize' ) );
 
 /**
  * Sanitization callback for saving the scaip_prevent_shortcode_addition post meta option.
  */
 function scaip_prevent_shortcode_addition_sanitize( $args ) {
 	$args = sanitize_text_field( $args );
-	if ( $args === 'on' ) {
+	if ( 1 === intval( $args ) ) {
 		$ret = $args;
 	} else {
 		$ret = false;
@@ -62,26 +57,37 @@ function scaip_prevent_shortcode_addition_sanitize( $args ) {
 
 /**
  * Save the options set in the metabox.
+ *
+ * @param int    $post_id the post ID.
+ * @param object $post the post object.
  */
-function _scaip_meta_box_save( $post_id ) {
-	global $post;
+function _scaip_meta_box_save( $post_id, $post ) {
 
-	// Bail if we're doing an auto save
+	// Verify the nonce before proceeding.
+	if ( ! isset( $_POST['scaip_metabox_nonce'] ) || ! wp_verify_nonce( $_POST['scaip_metabox_nonce'], 'scaip_metabox' ) ) {
+		return false;
+	}
+
+	// Bail if we're doing an auto save.
 	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 		return;
 	}
 
-	// if our current user can't edit this post, bail
+	// If our current user can't edit this post, bail.
 	if ( ! current_user_can( 'edit_post' ) ) {
 		return;
 	}
 
-	$value = $_POST['scaip_prevent_shortcode_addition'];
+	$new_meta_value = ( isset( $_POST['scaip_prevent_shortcode_addition'] ) ? sanitize_html_class( wp_unslash( $_POST['scaip_prevent_shortcode_addition'] ) ) : '' );
 
-	if ( get_post_meta( $post->ID, 'scaip_prevent_shortcode_addition', false ) ) {
-		update_post_meta( $post->ID, 'scaip_prevent_shortcode_addition', $value ); //if the custom field already has a value, update it
+	/*
+	 * If the checkbox was checked, update the meta_value
+	 * If the checkbox was unchecked, delete the meta_value
+	 */
+	if ( ! empty( $new_meta_value ) ) {
+		add_post_meta( $post_id, 'scaip_prevent_shortcode_addition', 1, true );
 	} else {
-		add_post_meta( $post->ID, 'scaip_prevent_shortcode_addition', $value );//if the custom field doesn't have a value, add the data
+		delete_post_meta( $post_id, 'scaip_prevent_shortcode_addition' );
 	}
 }
-add_action( 'save_post', '_scaip_meta_box_save' );
+add_action( 'save_post', '_scaip_meta_box_save', 10, 2 );
