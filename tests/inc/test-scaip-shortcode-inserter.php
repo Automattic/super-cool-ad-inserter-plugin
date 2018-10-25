8f6cdcf2640a90c549289341e15738200c3191f2
Ben Keith <ben@benlk.com>
2018-10-25 00:16:08 -0400

    separate the functionality in the shortcode insertion funciton into two separate functions: insertion and determination fo whether to insert. Add a filter for https://github.com/INN/super-cool-ad-inserter-plugin/issues/25 so that people may turn off insertion programmatically.

--- tests/inc/test-scaip-shortcode-inserter.php
+++ tests/inc/test-scaip-shortcode-inserter.php
@@ -12,4 +12,15 @@ class ScaipShortcodeInserterTestFunctions extends WP_UnitTestCase {
 		$this->markTestIncomplete( 'This test has not been implemented yet.' );
 	}
 
+	function test_scaip__maybe_insert_shortcode() {
+		// Check that it does nothing on posts outside The Loop
+		// check that the filter works
+		// check that it doesn't work on posts of other post tyles
+
+		$ret = scaip_maybe_insert_shortcode( '' );
+		$this->assertEquals( $ret, '' );
+
+		$this->markTestIncomplete( 'This test has not been implemented yet.' );
+	}
+
 }
