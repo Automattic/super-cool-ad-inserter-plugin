132ce506ca2b62165440eefd9623291485e6a836
Ben Keith <ben@benlk.com>
2018-10-29 18:00:40 -0400

    Add InspectorControls for block, with link to plugin docs.

--- blocks/scaip-sidebar/block.js
+++ blocks/scaip-sidebar/block.js
@@ -20,11 +20,15 @@
 	 */
 	var SelectControl = wp.components.SelectControl;
 	/**
+	 * Sidebar controls for the block
+	 * @link https://github.com/WordPress/gutenberg/tree/master/packages/editor/src/components/inspector-controls
+	 */
+	var InspectorControls = wp.editor.InspectorControls;
+	/**
 	 * Literally just for a fancy dashicon
 	 * @see https://github.com/WordPress/gutenberg/blob/master/packages/components/src/dashicon/README.md
 	 */
 	var dashicon = wp.components.Dashicon;
-
 	/**
 	 * Every block starts by registering a new block type definition.
 	 * @see https://wordpress.org/gutenberg/handbook/block-api/
@@ -93,6 +97,29 @@
 							onChange: function( value ) { props.setAttributes( { number: value } ); },
 						}
 					)
+				),
+				el( InspectorControls, {},
+					el(
+						SelectControl,
+						{
+							label: [
+								__( 'Inserted Ad Position:' )
+							],
+							options: options_array,
+							value: props.attributes.number,
+							onChange: function( value ) { props.setAttributes( { number: value } ); },
+							help: [
+								__( 'Which Inserted Ad Position sidebar should be displayed in this area? ' ), // trailing space is important.
+								el(
+									'a',
+									{
+										href: 'https://github.com/INN/super-cool-ad-inserter-plugin/blob/master/docs/configuration.md'
+									},
+									'View the documentation.'
+								),
+							],
+						}
+					),
 				),
 				// @todo InspectorControls with a help thing pointing users to the widget settings.
 			];
