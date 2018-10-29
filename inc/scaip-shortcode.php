Automattic/super-cool-ad-inserter-plugin

commit 5d85fa5f814602a23f67d7af26ca3a89310c2fb7

WIP on block

inc/scaip-shortcode.php
@@ -25,7 +25,14 @@ add_shortcode( 'ad', 'scaip_shortcode' );
  */
 function scaip_shortcode_do_sidebar( $args ) {
 	if ( isset( $args['number'] ) ) {
-		echo '<aside class="scaip scaip-' . esc_attr( $args['number'] ) . '">';
+		sprintf(
+			'<aside class="scaip scaip-%1$s %2$s %3$s %4$s %5$s">',
+			esc_attr( $args['number'] ),
+			( isset( $atts['align'] ) ) ? $atts['align'] : '',
+			( isset( $atts['class'] ) ) ? $atts['class'] : '',
+			( isset( $atts['className'] ) ) ? $atts['className'] : '',
+			( isset( $atts['customClassName'] ) ) ? $atts['customClassName'] : ''
+		);
 		dynamic_sidebar( 'scaip-' . $args['number'] );
 		echo '</aside>';
 	}

blocks/scaip-sidebar.php
@@ -1,0 +1,59 @@
+<?php
+/**
+ * Functions to register client-side assets (scripts and stylesheets) for the
+ * Gutenberg block.
+ *
+ * @package super-cool-ad-inserter-plugin
+ */
+
+/**
+ * Registers all block assets so that they can be enqueued through Gutenberg in
+ * the corresponding context.
+ *
+ * @see https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type/#enqueuing-block-scripts
+ */
+function scaip_sidebar_block_init() {
+	$dir = dirname( __FILE__ );
+
+	$block_js = 'scaip-sidebar/block.js';
+	wp_register_script(
+		'scaip-sidebar-block-editor',
+		plugins_url( $block_js, __FILE__ ),
+		array(
+			'wp-blocks',
+			'wp-i18n',
+			'wp-element',
+		),
+		filemtime( "$dir/$block_js" )
+	);
+
+	// Provide SCAIP's settings to the editor JS
+	$scaip_settings = array(
+		'period' => get_option( 'scaip_settings_period', 3 ),
+		'repetitions' => get_option( 'scaip_settings_repetitions', 2 ),
+		'minimum_paragraphs' => get_option( 'scaip_settings_min_paragraphs', 6 ),
+	);
+	wp_localize_script(
+		'scaip-sidebar-block-editor',
+		'scaip',
+		$scaip_settings
+	);
+
+	$editor_css = 'scaip-sidebar/editor.css';
+	wp_register_style(
+		'scaip-sidebar-block-editor',
+		plugins_url( $editor_css, __FILE__ ),
+		array(
+			'wp-blocks',
+		),
+		filemtime( "$dir/$editor_css" )
+	);
+
+	register_block_type( 'super-cool-ad-inserter-plugin/scaip-sidebar', array(
+		'editor_script' => 'scaip-sidebar-block-editor',
+		'editor_style'  => 'scaip-sidebar-block-editor',
+		'style'         => 'scaip-sidebar-block',
+		'render_callback' => 'scaip_shortcode',
+	) );
+}
+add_action( 'init', 'scaip_sidebar_block_init' );

blocks/scaip-sidebar/block.js
@@ -1,0 +1,110 @@
+( function( wp ) {
+	/**
+	 * Registers a new block provided a unique name and an object defining its behavior.
+	 * @see https://github.com/WordPress/gutenberg/tree/master/blocks#api
+	 */
+	var registerBlockType = wp.blocks.registerBlockType;
+	/**
+	 * Returns a new element of given type. Element is an abstraction layer atop React.
+	 * @see https://github.com/WordPress/gutenberg/tree/master/element#element
+	 */
+	var el = wp.element.createElement;
+	/**
+	 * Retrieves the translation of text.
+	 * @see https://github.com/WordPress/gutenberg/tree/master/i18n#api
+	 */
+	var __ = wp.i18n.__;
+	/**
+	 * Dropdown <select> element
+	 * @link https://github.com/WordPress/gutenberg/tree/master/packages/components/src/select-control
+	 */
+	var SelectControl = wp.components.SelectControl;
+	/**
+	 * Literally just for a fancy dashicon
+	 * @see https://github.com/WordPress/gutenberg/blob/master/packages/components/src/dashicon/README.md
+	 */
+	var dashicon = wp.components.Dashicon;
+
+	/**
+	 * Every block starts by registering a new block type definition.
+	 * @see https://wordpress.org/gutenberg/handbook/block-api/
+	 */
+	registerBlockType( 'super-cool-ad-inserter-plugin/scaip-sidebar', {
+		title: __( 'Inserted Ad Position Sidebar' ),
+		icon: 'welcome-widgets-menus',
+		category: 'widgets',
+		supports: {
+			html: false,
+			align: true,
+			anchor: false,
+			alignWide: true,
+			customClassName: true,
+			className: true,
+			multiple: true,
+		},
+
+		/**
+		 * The edit function describes the structure of your block in the context of the editor.
+		 * This represents what the editor will render when the block is used.
+		 * @see https://wordpress.org/gutenberg/handbook/block-edit-save/#edit
+		 *
+		 * @param {Object} [props] Properties passed from the editor.
+		 * @return {Element}       Element to render.
+		 */
+		edit: function( props ) {
+			if ( ! window.scaip ) {
+				return "Something is wrong with the Super Cool Ad Inserter Plugin.";
+			}
+
+			options_array=[];
+			for ( var i = 0; i < window.scaip.repetitions; i++ ) {
+				options_array.push( {
+					label: i,
+					value: i
+				} );
+			}
+
+			return [
+				el(
+					'div',
+					{
+						className: props.className,
+						align: props.align,
+					},
+					el(
+						SelectControl,
+						{
+							label: [
+								el(
+									dashicon,
+									{
+										icon: 'welcome-widgets-menus'
+									},
+								),
+								__( 'Inserted Ad Position:' )
+							],
+							options: options_array,
+							value: props.attributes.no,
+							onChange: function( value ) { props.setAttributes( { no: value } ); },
+						}
+					)
+				),
+				// @todo InspectorControls with a help thing pointing users to the widget settings.
+			];
+		},
+
+		/**
+		 * The save function defines the way in which the different attributes should be combined
+		 * into the final markup, which is then serialized by Gutenberg into `post_content`.
+		 * @see https://wordpress.org/gutenberg/handbook/block-edit-save/#save
+		 *
+		 * @return {Element}       Element to render.
+		 */
+		save: function( attributes ) {
+			// Rendering in PHP
+			return '';
+		}
+	} );
+} )(
+	window.wp
+);

blocks/scaip-sidebar/editor.css
@@ -1,0 +1,35 @@
+/**
+ * The following styles get applied inside the editor only.
+ *
+ * Replace them with your own styles or remove the file completely.
+ */
+
+.wp-block-super-cool-ad-inserter-plugin-scaip-sidebar {
+  background-color: #f8f9f9;
+  padding: 14px;
+}
+
+.wp-block-super-cool-ad-inserter-plugin-scaip-sidebar .components-base-control__field {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 0;
+}
+.wp-block-super-cool-ad-inserter-plugin-scaip-sidebar .components-base-control__field label {
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
+  white-space: nowrap;
+  font-weight: 600;
+  flex-shrink: 0;
+
+  margin-bottom: 0;
+
+  font-size: 13;
+  font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif;
+}
+.wp-block-super-cool-ad-inserter-plugin-scaip-sidebar .dashicon {
+  margin-right: 10px;
+}
+.wp-block-super-cool-ad-inserter-plugin-scaip-sidebar .components-base-control__field label {
+  flex-grow: 1;
+}

inc/scaip-shortcode-inserter.php
@@ -47,6 +47,14 @@ function scaip_insert_shortcode( $content = '' ) {
 		return $content;
 	}
 
+	/*
+	 * If we have a Gutenberg Block, bail
+	 * @uses has_block https://github.com/WordPress/gutenberg/issues/3773
+	 */
+	if ( function_exists( 'has_block' ) && has_block( 'super-cool-ad-inserter-plugin/scaip-sidebar', $content ) ) {
+		return $content;
+	}
+
 	$scaip_period = get_option( 'scaip_settings_period', 3 );
 	$scaip_repetitions = get_option( 'scaip_settings_repetitions', 2 );
 	$scaip_minimum_paragraphs = get_option( 'scaip_settings_min_paragraphs', 6 );

scaip.php
@@ -19,6 +19,7 @@ define( 'SCAIP_PLUGIN_FILE', __FILE__ );
  */
 require_once( __DIR__ . '/inc/scaip-shortcode-inserter.php' );
 require_once( __DIR__ . '/inc/scaip-shortcode.php' );
+require_once( __DIR__ . '/blocks/scaip-sidebar.php' );
 
 /**
  * Settings

commit fb46a4bd14a4d3fac7238389bed6ecebe9c87730

printf instead of sprintf so that the HTML balances, fixing bug introduced in 5d85fa5f814602a23f67d7af26ca3a89310c2fb7 of this branch.

inc/scaip-shortcode.php
@@ -27,7 +27,7 @@ add_shortcode( 'ad', 'scaip_shortcode' );
  */
 function scaip_shortcode_do_sidebar( $args ) {
 	if ( isset( $args['number'] ) ) {
-		sprintf(
+		printf(
 			'<aside class="scaip scaip-%1$s %2$s %3$s %4$s %5$s">',
 			esc_attr( $args['number'] ),
 			( isset( $args['align'] ) ) ? 'align' . $args['align'] : '',